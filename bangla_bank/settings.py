da87a54b78cd540dd02caff6988e11916c358860
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-07 19:05:56 +0600

    almost done this

--- bangla_bank/settings.py
+++ bangla_bank/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y8=6-k4w!8brl8qnq$@vu+$*&w$u39u4mw+3y&-x@t&mdr4-do"
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 
 # Application definition
 
@@ -40,6 +52,7 @@ INSTALLED_APPS = [
     "accounts",
     "core",
     "transactions",
+    "django.contrib.humanize",
 ]
 
 MIDDLEWARE = [
--- transactions/forms.py
+++ transactions/forms.py
@@ -1,23 +1,88 @@
-from django.db import models
-from accounts.models import UserBankAccount
-
-# Create your models here.
-from .constants import TRANSACTION_TYPE
+from django import forms
+from .models import Transactions
 
 
-class Transactions(models.Model):
-    account = models.ForeignKey(
-        UserBankAccount, related_name="transactions", on_delete=models.CASCADE
-    )
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transactions
+        fields = ["amount", "transaction_type"]
 
-    amount = models.DecimalField(decimal_places=2, max_digits=12)
-    balance_after_transaction = models.DecimalField(decimal_places=2, max_digits=12)
-    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    loan_approve = models.BooleanField(default=False)
+    def __init__(self, *args, **kwargs):
+
+        self.account = kwargs.pop("account", None)
+        super().__init__(*args, **kwargs)
+
+        self.fields["transaction_type"].disabled = True
+        self.fields["transaction_type"].widget = forms.HiddenInput()
+
+    def save(self, commit=True):
+
+        self.instance.account = self.account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save(commit=commit)
+
+
+class DepositForm(TransactionForm):
+    def clean_amount(self):  # amount field ke filter krbo...
+        min_deposit_amount = 100
+        amount = self.cleaned_data.get(
+            "amount"
+        )  # user er filap kra form theke amra amount field er value niye aslam...
+        if amount < min_deposit_amount:
+            raise forms.ValidationError(
+                f"You need to deposit at least {min_deposit_amount}$"
+            )
+        return amount
+
+
+class WithdrawalForm(TransactionForm):
+    def clean_amount(self):  # clean diye je fie
+        account = self.account
+        min_withdraw_balance = 50
+        max_withdraw_balance = 25000
+        balance = account.balance
+        amount = self.cleaned_data.get("amount")
+        if amount < min_withdraw_balance:
+            raise forms.ValidationError(
+                f"You can withdraw at least {min_withdraw_balance}$"
+            )
+        if amount > max_withdraw_balance:
+            raise forms.ValidationError(
+                f"You can withdraw maximum {max_withdraw_balance}$"
+            )
+        if amount > balance:
+            raise forms.ValidationError(
+                f"You have {balance}$ in you account"
+                "You can not withdraw more than your account balance"
+            )
+        return amount
+
+
+class LoanRequestForm(TransactionForm):
+    def clean_amount(self):
+        amount = self.cleaned_data.get("amount")
+        return amount
+
+
+class SendMoneyForm(forms.ModelForm):
+    account_no = forms.IntegerField(label="Account Number")
 
     class Meta:
-        ordering = ["timestamp"]
+        model = Transactions
+        fields = ["amount"]
 
-    def __str__(self):
-        return f"Account No: {self.account.account_no}"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {
+                    "class": (
+                        "appearance-none block w-full bg-gray-200 "
+                        "text-gray-700 border border-gray-200 rounded "
+                        "py-3 px-4 leading-tight focus:outline-none "
+                        "focus:bg-white focus:border-gray-500"
+                    ),
+                    "placeholder": ("Please fill this input is correctly...."),
+                }
+            )
