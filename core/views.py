a97b679781a73593691e0be7161ed718817991d3
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-07 17:13:00 +0600

    added all data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+
+
+class HOMEVIEW(TemplateView):
+    template_name = "index.html"
